677fc209c61f2a78fb652ea5b84120d5a2f43b99
stumburs <70687973+stumburs@users.noreply.github.com>
2023-03-10 02:27:08 +0200

    Update bf.cpp
    Actually fixed new line errors.

--- bf.cpp
+++ bf.cpp
@@ -28,12 +28,14 @@ int main(int argc, char *argv[])
     std::string program((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
     std::erase_if(program, [](char c)
                   { return !strchr("><+-.,[]", c); });
+    input_file.close();
 
     // Init memory
     unsigned char mem[30000] = {0}, *ptr = mem;
 
     // Run program
     std::stack<std::streampos> loop_stack; // stack to keep track of loop positions
+    std::size_t loop_counter = 0;          // counter for nested loops
     std::size_t i = 0;
     while (i < program.length())
     {
@@ -58,16 +60,40 @@ int main(int argc, char *argv[])
             *ptr = getchar();
             break;
         case '[':
-            if (*ptr != 0)
+            if (*ptr == 0)
+            {
+                // Skip loop body
+                int nested_loops = 1;
+                while (nested_loops > 0)
+                {
+                    ++i;
+                    if (program[i] == '[')
+                        ++nested_loops;
+                    else if (program[i] == ']')
+                        --nested_loops;
+                }
+            }
+            else
+            {
                 loop_stack.push(i);
+                ++loop_counter;
+            }
             break;
         case ']':
-            *ptr ? void(i = loop_stack.top()) : loop_stack.pop();
+            if (*ptr != 0)
+                i = loop_stack.top();
+            else
+            {
+                loop_stack.pop();
+                --loop_counter;
+            }
+            break;
+        default:
             break;
         }
         ++i;
+        if (loop_counter == 0 && i >= program.length())
+            break; // exit loop if all loops have been exited
     }
-
-    input_file.close();
     return 0;
 }
